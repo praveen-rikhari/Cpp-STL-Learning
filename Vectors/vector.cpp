7734aa5e62a65efdc19e85b92f546a6c2d4487ee
praveen-rikhari <praveenrikhari45@gmail.com>
2024-06-23 23:21:16 +0530

    Implementing vectors in STL

---
+++ Vectors/vector.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void explain_vector()
+{
+    // Create vectors
+    vector<int> vec1;
+    vector<int> vec2(10);    // vector of size 10;
+    vector<int> vec3(10, 5); // vector of size 10 with all initialized with 5
+    vector<int> v = {1, 2, 3, 4, 5};
+
+    // Access elements
+    cout << "Element at index 0: " << v[0] << endl;
+    cout << "Element at index 1: " << v.at(1) << endl;
+
+    // Modify elements
+    v[0] = 10;
+    v.at(1) = 20;
+
+    // Add elements
+    vec1.push_back(6);
+    vec1.push_back(7);
+
+    // Remove elements
+    vec1.pop_back();
+}
+
+int main()
+{
+    explain_vector();
+    return 0;
+}
